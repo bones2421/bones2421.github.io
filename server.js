5aa8fe96f90cb385688f67756261d8e8457c267c
Dormin <williamsben91@gmail.com>
2023-09-16 05:51:07 -0600

    added dependencies and node.js stuff, added express stuff, added server stuff for email server, added gitignore.

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+// Define a route for the root path
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+
+app.post('/send-email', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: `Contact Form Submission from ${name}`,
+    text: `From: ${name} (${email})\n\nMessage:\n${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('Email sent');
+  } catch (error) {
+    res.status(500).send('An error occurred');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000/');
+});
