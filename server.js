477950c236f0be1d515e79b44d89a8ae50ae230e
Dormin <williamsben91@gmail.com>
2023-09-16 06:11:16 -0600

    added dotenv dependencies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
