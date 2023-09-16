ef8b1cb36d40a1e50dd12de10a01efa1dfbf3fd9
Dormin <williamsben91@gmail.com>
2023-09-16 06:00:23 -0600

    update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
